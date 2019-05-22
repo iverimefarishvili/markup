db299ed524fbe154a63ea7fbc7ee6fb3a19182f6
iverimefarishvili <34187156+iverimefarishvili@users.noreply.github.com>
2019-05-22 13:52:31 -0400

    Add files via upload

---
+++ saatec-task/app.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn");
+let dropdowns = document.getElementById("ul");
+btn.addEventListener("click", function(){
+    
+    dropdowns.classList.toggle("active");
+})
+
